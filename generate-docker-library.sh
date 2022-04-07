02e367644b7e9306dc818ee1224f7e1005b41998
Miguel Moquillon <miguel.moquillon@silverpeas.com>
2022-04-07 16:20:03 +0200

    update the key server host

--- generate-docker-library.sh
+++ generate-docker-library.sh
@@ -11,6 +11,7 @@ function printVersion() {
 
 Tags: $1
 GitCommit: $2
+GitFetch: refs/heads/$3
 	EOE
 }
 
@@ -29,10 +30,11 @@ for version in `git tag | tac | grep "^[0-9.]\+$"`; do
 
   count=$(( count + 1 ))
   commit=`git rev-parse ${version}`
+  fetch=`echo ${version} | grep -o "[0-9].[0-9]"`.x
   if [ $isFirst -eq 1 ]; then
     isFirst=0
-    printVersion "${version}, latest" ${commit}
+    printVersion "${version}, latest" ${commit} ${fetch}
   else
-    printVersion "${version}" ${commit}
+    printVersion "${version}" ${commit} ${fetch}
   fi
 done
